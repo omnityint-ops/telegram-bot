92a486bf48d0f1fb8063d9c9d9d847b06707cde2
omnityint-ops <omnityint@gmail.com>
2025-09-02 00:22:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -756,7 +756,7 @@ async def on_draw_lemon(mv: MatchView):
         pass
 
 
-@router.message(Command("addstars"))
+@dp.message(Command("addstars"))
 async def cmd_addstars(m: Message):
     if not is_admin(m.from_user.id):
         return await m.answer("⛔ Нет доступа")
